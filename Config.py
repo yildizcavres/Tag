0563b48bd77774c7f77360f1fa33a650b85ac079
yildizcavres <129795323+yildizcavres@users.noreply.github.com>
2023-04-15 04:34:50 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,13 +4,13 @@ import os
 class Config():
     # Bu dəyərləri my.telegram.org saytından əldə edin
     #>>> https://my.telegram.org
-    API_ID = int(os.environ.get("API_ID","16157584"))
-    API_HASH = os.environ.get("API_HASH","2167d4e6007a79eed91d084bf5b8966a")
+    API_ID = int(os.environ.get("API_ID",""))
+    API_HASH = os.environ.get("API_HASH","")
     BOT_TOKEN = os.environ.get("BOT_TOKEN","5337183930:")
     BOT_USERNAME = os.environ.get("BOT_USERNAME","dejavy")
-    BOT_NAME = os.environ.get("BOT_NAME","jhkoijl")
-    BOT_ID = int(os.environ.get("BOT_ID","234567890-"))
-    SUDO_USERS = os.environ.get("SUDO_USERS","rseyutiydou").split()
+    BOT_NAME = os.environ.get("BOT_NAME","goldtag")
+    BOT_ID = int(os.environ.get("BOT_ID","-"))
+    SUDO_USERS = os.environ.get("SUDO_USERS","").split()
     SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT","ioj")
-    OWNER_ID = int(os.environ.get("OWNER_ID","43567890"))
+    OWNER_ID = int(os.environ.get("OWNER_ID","5154387018"))
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME","uijio;kl,")
